cf6a4d3f276c6b64ac91f3e981a6b722f5894098
Mka91 <kiwogens@yandex.ru>
2021-11-24 22:08:05 +0700

    PhoneBook

---
+++ src/main/java/ru/academits/PhoneBookSpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.academits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookSpringHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookSpringHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/academits/PhoneBookSpringHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.academits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookSpringHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
